0623c6605c4de90a9f47c188722ab1e24e29075f
barbwirezeuz <soceeenin@yahoo.com>
2023-09-21 13:32:43 +0100

    changes

--- src/Signin.js
+++ src/Signin.js
@@ -1,70 +1,4 @@
-// import React, { useState } from "react";
-// import { signInWithFirebase } from "./firebase";
-// import { useNavigate } from "react-router-dom";
 
-// function SignIn() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [successMessage, setSuccessMessage] = useState("");
-//   const [errorMessage, setErrorMessage] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSignIn = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       const user = await signInWithFirebase(email, password);
-
-//       // If authentication is successful, set success message
-//       setSuccessMessage("Sign-in successful!");
-
-//       // Clear the success message after 3 seconds
-//       setTimeout(() => {
-//         setSuccessMessage("");
-//       }, 3000);
-
-//       // Redirect to the dashboard page upon successful sign-in
-//       navigate("/dashboard");
-//     } catch (error) {
-//       console.error(error);
-//       // Handle authentication errors here
-//       // ...
-
-//       // Set error message
-//       setErrorMessage("Sign-in failed. Please check your credentials.");
-
-//       // Clear the error message after 3 seconds
-//       setTimeout(() => {
-//         setErrorMessage("");
-//       }, 3000);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Sign In</h2>
-//       <form onSubmit={handleSignIn}>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <button type="submit">Sign In</button>
-//       </form>
-//       {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
-//       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-//     </div>
-//   );
-// }
-
-// export default SignIn;
 
 
 
@@ -88,25 +22,18 @@ function SignIn() {
     try {
       const user = await signInWithFirebase(email, password);
 
-      // If authentication is successful, set success message
       setSuccessMessage("Sign-in successful!");
 
-      // Clear the success message after 3 seconds
       setTimeout(() => {
         setSuccessMessage("");
       }, 3000);
 
-      // Redirect to the dashboard page upon successful sign-in
       navigate("/dashboard");
     } catch (error) {
       console.error(error);
-      // Handle authentication errors here
-      // ...
-
-      // Set error message
+      
       setErrorMessage("Sign-in failed. Please check your credentials.");
 
-      // Clear the error message after 3 seconds
       setTimeout(() => {
         setErrorMessage("");
       }, 3000);
